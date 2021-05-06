714dede550fcc5bf37c379bcc53030c199a7aa17
Arunabh Das <arunabhdas@gmail.com>
2021-05-06 02:59:53 -0400

    Added setter and getter for fullname

--- deuce.py
+++ deuce.py
@@ -2,6 +2,9 @@ import employee
 
 if __name__ == '__main__':
     emp = employee.Employee('John', 'Smith')
-    print(emp.fullname())
+    print(emp.fullname)
+    emp.fullname = 'Jane Doe'
+    print(emp.email)
+    print(emp.fullname)
 
 
--- employee.py
+++ employee.py
@@ -3,8 +3,18 @@ class Employee:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + "." + last + '@email.com' 
+        self.email = first + "." + last + '@email.com'
 
+    def email(self):
+        return '{} {}'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
