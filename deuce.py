c1cebe28b2dc4c39cf0d6044ff74d36033b9853a
Arunabh Das <arunabhdas@gmail.com>
2021-05-06 02:49:39 -0400

    Improved deuce.py

--- deuce.py
+++ deuce.py
@@ -1,7 +1,7 @@
 import employee
 
-if __name__ == "__main__":
-    emp = employee.Employee("John", "Smith")
+if __name__ == '__main__':
+    emp = employee.Employee('John', 'Smith')
     print(emp.fullname())
 
 
